02c836d24169d6e56de0d7c6fc026157d356391b
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2021-07-16 18:33:41 +0200

    update tasks

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -35,17 +35,20 @@ async function routes(fastify) {
 
   fastify.patch('/:id', {schema: updateTask}, async (request, reply) => {
     const {id} = request.params;
-    const {name} = request.body;
-    const query = {
-      text: `UPDATE tasks SET 
-                            name = COALESCE($1, name), 
-                            WHERE id = $2 RETURNING *`,
-      values: [name, id],
-    };
+    const {name,description} = request.body;
+
     try {
-      const {rows} = await client.query(query);
-      console.log(rows[0]);
-      reply.code(204);
+      const task = await models.Task.findOne(
+        {where: {
+          id: id
+        }}
+      )
+
+      if (task) {
+        task.update({description:description, name:name})
+        reply.send(task)
+      } 
+
     } catch (err) {
       throw new Error(err);
     }
@@ -54,13 +57,12 @@ async function routes(fastify) {
   fastify.delete('/:id', {schema: deleteTask}, async function(request, reply) {
     console.log(request.params)
     try {
-        console.log('QWERTY');
         await models.Task.destroy({
           where:{
             id: request.params.id
           }
         });
-        reply.code(204)
+        reply.send("deleted")
     } catch(err) {
             throw new Error(err)
     }
