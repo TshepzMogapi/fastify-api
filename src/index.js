0f8d1f187ae7115709bc22b938b1d70b87b04270
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2021-08-06 01:20:23 +0200

    set up docker

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true });
-const taskRoutes = require('./routes/tasks.routes');
-const dbconnector = require('./db/db');
+// const taskRoutes = require('./routes/tasks.routes');
+// const dbconnector = require('./db/db');
 
-fastify.register(dbconnector);
-fastify.register(taskRoutes);
+// fastify.register(dbconnector);
+// fastify.register(taskRoutes);
+fastify.register(require('./routes'));
 
 // Declare a route
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
+// fastify.get('/', async (request, reply) => {
+//   return { hello: 'world' }
+// })
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3009);
+    await fastify.listen(3000, '0.0.0.0');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
